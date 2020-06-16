d11523b4d640807b0c6a7c5aa8335dab9cd7558d
Jan Forster <jan.forster@de.ibm.com>
2020-06-16 12:48:34 +0200

    fixed typo in php script

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,13 +12,12 @@ if(empty($_POST['name'])  		||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-$message = $_POST['message'];
-$message = $_POST['ort'];
+$ort = $_POST['ort'];
 	
 // Create the email and send the message
 $to = 'info@galabau-forster-partner.de'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Webseiten Anfrage von:  $name";
-$email_body = "Neue Anfrage im Kontaktformular\n\nName: $name\n\nEmail: $email_address\n\Ort: $ort\n\Nachricht:\n$message";
+$email_body = "Neue Anfrage im Kontaktformular\n\nName: $name\n\nEmail: $email_address\n\nOrt: $ort\n\nNachricht:\n$message";
 $headers = "From: noreply@galabau-forster-partner.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
